078527bd2c8b9a96894917562d3457d846e04c5a
Adam Mwaniki <adamndegwa@protonmail.com>
2024-03-22 18:13:52 +0300

    Completes the scope lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob'
+//uppercase bob
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+//best customer
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+//
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+//least facourite character is unsuccessfully overwritten
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'jack'
+    return leastFavoriteCustomer= 'jane';
+}
